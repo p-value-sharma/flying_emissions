4cacad566a802da4c9ab4eff50c18d12d7c9dd4a
Prateek <prateeksharma.app@gmail.com>
2019-05-15 18:04:23 -0400

    more changes

---
+++ code/scraping_published_sheet.R
@@ -0,0 +1,7 @@
+library(rvest)
+
+link <- "https://docs.google.com/spreadsheets/d/e/2PACX-1vRHvr69sISXqaYL9DA0jeUl0TIe-OTXdSS7JgIQCfmL3jboMEEt7ZK-3L4hLPLeAuXw_y15xBSjD_hj/pubhtml?gid=0&single=true"
+
+read_html(link) %>% 
+  rvest::html_table()
+
